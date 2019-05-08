b9b1ae7ecdeab15587dc2536d2423e73a6ed4371
Kilian <Kilian@DESKTOP-1PIPS59>
2019-05-08 09:51:17 +0100

    Añadido FXML al proyecto

--- src/modelo/dao/AccesoDatos.java
+++ src/modelo/dao/AccesoDatos.java
@@ -9,15 +9,52 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import auxiliar.BaseDeDatos;
+import modelo.Equipo;
 
 public class AccesoDatos {
 
-	// 02 mayo 2019
-	public void insertar(String rutaFichero) {
+	//08 mayo 2019
+	
+	/*
+	 * public boolean validaLogin(String user, String pwd) { //conectar a la base de
+	 * datos //preparar consulta contra liga.usuarios(SELECT) //como saber si existe
+	 * o no ?
+	 * 
+	 * BaseDeDatos bd = new BaseDeDatos("localhost", "liga", "root", ""); Connection
+	 * conexion = bd.getConexion(); Statement stmt = conexion.createStatement();
+	 * ResultSet rS = stmt.executeQuery("SELECT * from usuario where 1");
+	 * ResultSetMetaData mD = rS.getMetaData();
+	 * 
+	 * return false;
+	 * 
+	 * }
+	 */
 
-	}
+	// 02 mayo 2019
+
+	/*
+	 * public ArrayList<Equipo> crearListaEquipoBD(String dbDatos, String tabla) {
+	 * ArrayList<Equipo> listaEquipo = new ArrayList<Equipo>(); BaseDeDatos bd = new
+	 * BaseDeDatos("localhost", "liga", "root", ""); Connection conexion =
+	 * bd.getConexion(); Statement stmt = conexion.createStatement(); ResultSet rst
+	 * = stmt.executeQuery("select * from" + tabla + "where 1"); ResultSetMetaData
+	 * rsMet = rst.getMetaData(); rsMet.getColumnCount(); while (rst.next()) { int
+	 * id = Integer.parseInt(rst.getString(1)); String nombreCorto =
+	 * rst.getString(2); String nombreLargo = rst.getString(3); int puntos =
+	 * Integer.parseInt(rst.getString(4)); int pg =
+	 * Integer.parseInt(rst.getString(5)); int pe =
+	 * Integer.parseInt(rst.getString(6)); int pp =
+	 * Integer.parseInt(rst.getString(7)); int gf =
+	 * Integer.parseInt(rst.getString(8)); int gc =
+	 * Integer.parseInt(rst.getString(9));
+	 * 
+	 * } return listaEquipo;
+	 * 
+	 * }
+	 */
 
 	// 30 abril 2019
 
